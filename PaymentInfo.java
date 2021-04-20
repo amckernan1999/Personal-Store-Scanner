4f7cb9b41ceadad3a18577dc093210869f61798e
Andrew McKernan <Amckernan19990@gmail.com>
2021-04-19 17:38:19 -0700

    Added constructor, Ready for testing

--- PaymentInfo.java
+++ PaymentInfo.java
@@ -1,10 +1,21 @@
-
+import java.util.*;
 public class PaymentInfo
 {
 	public String billingAddress;
 	public String nameOnCard;
 	public String zipcode;
-	public string expDate;
-	public string ccNum;
-	public bool saveInfo = false;
+	public String expDate;
+	public String ccNum;
+	public Bool saveInfo; 
+
+	public PaymentInfo()
+	{
+		billingAddress = "";
+		nameOnCard = "";
+		zipcode = "";
+		expDate = "";
+		ccNum = "";
+		saveInfo = false;
+	}
+	
 }
