a0f06b78e4d67b959328698371158642b41e4ea1
Andrew McKernan <Amckernan19990@gmail.com>
2021-04-22 13:34:10 -0700

    User class finished but untested

--- User.java
+++ User.java
@@ -9,50 +9,64 @@ public class User
 	private String username;
 	private String password;
 	
-	private ArrayList<Cart> cartHistory = new ArrayList<Cart>;
-	private ArrayList<PaymentInfo> savedPayment = new ArrayList<PaymentInfo>;
+	private ArrayList<Cart> cartHistory; 
+	private ArrayList<PaymentInfo> savedPayment;
 	
-	
+	//null constructor assigns default values
+	public User()
+	{
+		username = "";
+		password = "";
+		cartHistory = new ArrayList<Cart>();
+		savedPayment = new ArrayList<PaymentInfo>();
+	}
+
+	//stores string in field to save username
 	public void setUsername(String uName)
 	{
-		
+		username = uName;
 	}
 	
+	//stores string in field to save password
 	public void setPassword(String pWord) 
 	{
-		
+		password = pWord;
 	}
 	
-	public void checkUsername(string uName)
+	//compares by value saved username and input passed
+	public Bool checkUsername(String uName)
 	{
-		
+		return username.equals(uName);
 	}
 	
-	public void checkPassword(string pWord)
+	//compares by value saved password and input passed
+	public Bool checkPassword(String pWord)
 	{
-		
+		return password.equals(pword);
 	}
 	
+	//stores cart in array list cart history for purchase records
 	public void storeCartHistory(Cart info)
 	{
-		
+		cartHistory.add(info);
 	}
 	
+	// returns a cart in cart history for lookup later
 	public Cart getCartInHistory(int loc) 
 	{
-		Cart c = new Cart();
-		return c;
+		return cartHistory.get(loc);
 	}
 	
+	//saves a payment for later transactions
 	public void savePay(PaymentInfo pay)
 	{
-		
+		savedPayment.add(pay);
 	}
 	
+	//returns info of specific payment
 	public PaymentInfo getPayment(int loc)
 	{
-		PaymentInfo p = new PaymentInfo();
-		return p;
+		return PaymentInfo.get(loc);
 	}
-	}
+
 }
