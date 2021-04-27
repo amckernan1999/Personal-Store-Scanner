87201494a6d757048ef95a76bea7bd82d264feaf
amckernan1999 <44986722+amckernan1999@users.noreply.github.com>
2021-04-26 18:06:13 -0700

    Simple DB simulator for getting the view running.
    Will need to be changed for a real DB later.

---
+++ DummyDB.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class DummyDB
+{
+  public Item getItem(int code)
+  {
+      	Item canBeans = new Item("Black Beans", "12oz organic sustainably sourced Black Beans", 1.95);
+      	return canBeans;
+  }
+}
