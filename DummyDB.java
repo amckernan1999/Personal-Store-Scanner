5e94b086adcccf2cd507ffd0eae58e020f0aac5d
amckernan1999 <44986722+amckernan1999@users.noreply.github.com>
2021-04-28 12:54:42 -0700

    Updated to handle Item searches

--- DummyDB.java
+++ DummyDB.java
@@ -1,10 +1,31 @@
-import java.util.*;
 
-public class DummyDB
+import java.util.*;
+import java.io.*;
+
+public class DummyDB 
 {
-  public Item getItem(int code)
-  {
-      	Item canBeans = new Item("Black Beans", "12oz organic sustainably sourced Black Beans", 1.95);
-      	return canBeans;
-  }
+    
+    private String[][] itemTable = {
+        { "381370036012", "Aveeno Lotion", "18 Fl Oz daily moisturizer", "8.54"},
+        { "190198001757", "Lightning to 3.5MM Adapter", "Apple lightning to headphone jack adapter", "7.99"},
+        { "070330506060", "Bic White-out", "2 Pack quick dry correction fluid", "6.51"},
+        { "041508800082", "San Pelligrino Mineral water", "12oz Single Can", "1.45"},
+        { "787148244069", "Taylor Lane Organic Coffee",	"10oz Goat Rock Roast",	"17.99"}
+    };
+    
+    public Item getItem(String code) 
+    {
+     
+            
+            for(int i = 0; i < itemTable.length; i++)
+            {
+              if( itemTable[i][0].equals(code))
+              {
+                 return new Item(itemTable[i][1], itemTable[i][2], Double.parseDouble(itemTable[i][3]));
+              }
+            }
+      
+        return null;
+    }
+
 }
